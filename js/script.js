af25ba0f39d968b4a5b90e1482117807ff5cd0e8
zhLisha <lisazhou1709@gmail.com>
2022-06-01 14:59:25 +0200

    added toFixed(2) just in future case

--- js/script.js
+++ js/script.js
@@ -10,9 +10,11 @@ Il prezzo avra' un massimo di 2 decimali
 const userAge = prompt ('Inserisci la tua età');
 const userTrainKm = parseInt (prompt ('Inserisci i km che dovrai percorrere'));
 
+
 // Applicare prezzo intero in base ai km dell'utente
 const fixedPrice = 0.21;
-const fullPrice = userTrainKm * fixedPrice;
+let fullPrice = userTrainKm * fixedPrice;
+
 
 // Applicazione dello sconto per under 18 e over 65
 let discountOver65 = 65;
@@ -23,18 +25,30 @@ if (userAge < discoutUnder18) {
     discoutPrice = fullPrice - (fullPrice * 20 / 100);
 } else if (userAge > discountOver65) {
     discoutPrice = fullPrice - (fullPrice * 40 / 100);
+} else {
+    discoutPrice = fullPrice
 }
+
+
+/* 
+Riduzione 2 cifre decimali (non necessario in questo caso, ma 
+per casi futuri in cui le percentuali o il prezzo fisso dovessero 
+cambiare) 
+*/
+let decimalsNumber = discoutPrice.toFixed(2);
+
 
 // Messaggio all'user\
 let userMessage;
 
 if (userAge < discoutUnder18) {
-    userMessage = 'Con lo sconto del 20%, il totale è: ' + discoutPrice + '€';
+    userMessage = 'Con lo sconto del 20%, il totale è: ' + decimalsNumber + '€';
 } else if (userAge > discountOver65) {
-    userMessage = 'Con lo sconto del 40%, il totale è: ' + discoutPrice  + '€';
+    userMessage = 'Con lo sconto del 40%, il totale è: ' + decimalsNumber + '€';
 } else {
-    userMessage = 'Il totale è: ' + fullPrice  + '€';
+    userMessage = 'Il totale è: ' + decimalsNumber  + '€';
 }
+
 
 // Stampa
 alert(userMessage)
